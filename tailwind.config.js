238a66b0f1724fffbbbcaddcd34e18b71c73b190
Ajit Tupe <ajittupe2548@gmail.com>
2024-08-23 23:28:30 +0530

    Added: Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/app/**/*.{js,jsx,ts,tsx,mdx}',
+    './src/components/**/*.{js,jsx,ts,tsx,mdx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
