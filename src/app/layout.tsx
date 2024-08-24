6e14f9a6459c9297d3d3ab052bff9c739f34a815
Ajit Tupe <ajittupe2548@gmail.com>
2024-08-24 16:07:49 +0530

    Added: Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,32 @@
+import Navbar from '@/components/Navbar';
+import type { Metadata } from 'next';
+import { ThemeProvider } from '@/components/ThemeProvider';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: {
+        template: 'Ajit Tupe',
+        default: 'Ajit Tupe',
+    },
+    description: 'My portfolio (web developer)',
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang='en'>
-            <body>{children}</body>
+            <body className={inter.className}>
+                <ThemeProvider attribute='class'>
+                    <Navbar />
+                    <main className='p-4 pb-12 max-w-7xl mx-auto overflow-hidden lg:overflow-visible'>
+                        {children}
+                    </main>
+                </ThemeProvider>
+            </body>
         </html>
     );
-}
+};
+
+export default RootLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
     content: [
         './src/app/**/*.{js,jsx,ts,tsx,mdx}',
         './src/components/**/*.{js,jsx,ts,tsx,mdx}',
