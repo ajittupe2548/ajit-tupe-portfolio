a6dc3d07c149c80ec7575830aee7fe7e4e96f42e
Ajit Tupe <ajittupe2548@gmail.com>
2024-08-25 12:56:42 +0530

    Integrated: react-icons

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Github, Linkedin, Facebook, Twitter } from 'lucide-react';
+import { FaLinkedin, FaGithub, FaFacebook, FaXTwitter } from 'react-icons/fa6';
 
 const AboutMe: React.FC = () => {
     return (
@@ -24,38 +24,28 @@ const AboutMe: React.FC = () => {
                                 className='transition-all text-pink-500 hover:scale-125 duration-300'
                                 href='https://github.com/ajittupe2548'
                             >
-                                <Github height={30} width={30} />
+                                <FaGithub size={30} />
                             </a>
                             <a
                                 target='_blank'
                                 className='transition-all text-pink-500 hover:scale-125 duration-300'
                                 href='https://www.linkedin.com/in/ajittupe2548/'
                             >
-                                <Linkedin height={30} width={30} />
+                                <FaLinkedin size={30} />
                             </a>
                             <a
                                 target='_blank'
                                 className='transition-all text-pink-500 hover:scale-125 duration-300'
                                 href='https://www.facebook.com/ajittupe2548/'
                             >
-                                <Facebook height={30} width={30} />
+                                <FaFacebook size={30} />
                             </a>
                             <a
                                 target='_blank'
                                 className='transition-all text-pink-500 hover:scale-125 duration-300'
                                 href='https://x.com/ajittupe2548'
                             >
-                                <svg
-                                    stroke='currentColor'
-                                    fill='currentColor'
-                                    strokeWidth='0'
-                                    viewBox='0 0 512 512'
-                                    height='30'
-                                    width='30'
-                                    xmlns='http://www.w3.org/2000/svg'
-                                >
-                                    <path d='M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z'></path>
-                                </svg>
+                                <FaXTwitter size={30} />
                             </a>
                         </div>
                     </div>
