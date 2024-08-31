bf607bf7931d8091dcea91471f323dd5f9fd3b84
Ajit Tupe <ajittupe2548@gmail.com>
2024-08-31 22:16:39 +0530

    Update ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -13,13 +13,11 @@ export default function ThemeToggle() {
         >
             <LuSun
                 size={22}
-                // style={{ display: theme !== 'dark' ? 'none' : 'block' }}
                 className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 hover:animate-rotate'
             />
             <LuMoon
                 size={22}
-                // style={{ display: theme === 'dark' ? 'none' : 'block' }}
-                className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'
+                className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 hover:animate-oscillate dark:animate-none'
             />
         </button>
     );
