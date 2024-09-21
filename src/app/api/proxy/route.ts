e8850d2ed17ab25242d283764ceace07a0c1e33a
Ajit Tupe <ajittupe2548@gmail.com>
2024-09-21 22:49:56 +0530

    Handled proxy for env variable

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -1,23 +1,28 @@
 export async function POST(request) {
-    try {
-        const formData = await request.json();
+    if (process.env.GOOGLE_SHEET_ID) {
+        try {
+            const formData = await request.json();
 
-        const url = `https://script.google.com/macros/s/${process.env.GOOGLE_SHEET_ID}/exec`;
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        });
+            const url = `https://script.google.com/macros/s/${process.env.GOOGLE_SHEET_ID}/exec`;
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
 
-        const data = await response.json();
+            const data = await response.json();
 
-        return new Response(JSON.stringify(data), {
-            status: response.status,
-            headers: { 'Content-Type': 'application/json' }
-        });
-    } catch (error) {
-        return new Response('Internal Server Error', { status: 500 });
+            return new Response(JSON.stringify(data), {
+                status: response.status,
+                headers: { 'Content-Type': 'application/json' }
+            });
+        } catch (error) {
+            return new Response('Internal Server Error', { status: 500 });
+        }
+    }
+    else {
+        return new Response('Ok', { status: 200 })
     }
 }
